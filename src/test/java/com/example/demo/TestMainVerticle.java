6565fcda3d38f32b22a4b08a142171614eccbdb3
Rafał Jeżewski <rafal.jezewski@gmail.com>
2020-10-19 17:13:04 +0200

    initial commit

---
+++ src/test/java/com/example/demo/TestMainVerticle.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import io.vertx.core.Vertx;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import java.util.concurrent.TimeUnit;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(VertxExtension.class)
+public class TestMainVerticle {
+    @Test
+    void httpServerRunnable() throws Throwable {
+        VertxTestContext testContext = new VertxTestContext();
+        Vertx vertx = Vertx.vertx();
+        vertx.deployVerticle(new MainVerticle(), testContext.completing());
+
+        assertThat(testContext.awaitCompletion(5, TimeUnit.SECONDS)).isTrue();
+        if (testContext.failed()) {
+            throw testContext.causeOfFailure();
+        }
+    }
+
+}
